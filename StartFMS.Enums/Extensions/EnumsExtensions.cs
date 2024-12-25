58b5269eb32e12c1e23b2eed64bf53d4cb6c445b
John Conte <information9527@gmail.com>
2024-12-25 10:16:30 +0800

    新增列舉擴充方法及系統名稱列舉
    在 `EnumsExtensions.cs` 中新增靜態類別 `EnumsExtensions`，包含多個擴充方法，用於取得列舉型別的 `DisplayAttribute` 屬性值，例如 `Name`、`Description`、`GroupName`、`Order`、`ShortName`、`Prompt`、`AutoGenerateField`、`AutoGenerateFilter`，以及取得列舉清單和其 `DisplayAttribute.Name` 的方法。
    
    在 `SystemNames.cs` 中新增列舉 `SystemNames`，並為每個列舉成員添加 `Display` 屬性，包含 `Name` 和 `Description`。
    
    在 `StartFMS.Enums.csproj` 中新增專案檔，設定目標框架為 `net9.0`，並啟用了隱式使用和可空性功能。

---
+++ StartFMS.Enums/Extensions/EnumsExtensions.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartFMS.Enums.Extensions
+{
+    static class EnumsExtensions
+    {
+        /// <summary>
+        /// 取得列舉的 DisplayAttribute.Name
+        /// </summary>
+        /// <param name="enumValue"></param>
+        /// <returns></returns>
+        public static string? GetDisplayName(this Enum enumValue)
+        {
+            var enumType = enumValue.GetType();
+            var memberInfo = enumType.GetMember(enumValue.ToString());
+            var displayAttribute = memberInfo[0].GetCustomAttributes(typeof(System.ComponentModel.DataAnnotations.DisplayAttribute), false).FirstOrDefault() as System.ComponentModel.DataAnnotations.DisplayAttribute;
+            return displayAttribute?.Name;
+        }
+
+        /// <summary>
+        /// 取得列舉的 DisplayAttribute.Description
+        /// </summary>
+        /// <param name="enumValue"></param>
+        /// <returns></returns>
+        public static string? GetDescription(this Enum enumValue)
+        {
+            var enumType = enumValue.GetType();
+            var memberInfo = enumType.GetMember(enumValue.ToString());
+            var displayAttribute = memberInfo[0].GetCustomAttributes(typeof(System.ComponentModel.DataAnnotations.DisplayAttribute), false).FirstOrDefault() as System.ComponentModel.DataAnnotations.DisplayAttribute;
+            return displayAttribute?.Description;
+        }
+
+        /// <summary>
+        /// 取得列舉的 DisplayAttribute.GroupName
+        /// </summary>
+        /// <param name="enumValue"></param>
+        /// <returns></returns>
+        public static string? GetGroupName(this Enum enumValue)
+        {
+            var enumType = enumValue.GetType();
+            var memberInfo = enumType.GetMember(enumValue.ToString());
+            var displayAttribute = memberInfo[0].GetCustomAttributes(typeof(System.ComponentModel.DataAnnotations.DisplayAttribute), false).FirstOrDefault() as System.ComponentModel.DataAnnotations.DisplayAttribute;
+            return displayAttribute?.GroupName;
+        }
+
+        /// <summary>
+        /// 取得列舉的 DisplayAttribute.Order
+        /// </summary>
+        /// <param name="enumValue"></param>
+        /// <returns></returns>
+        public static int GetOrder(this Enum enumValue)
+        {
+            var enumType = enumValue.GetType();
+            var memberInfo = enumType.GetMember(enumValue.ToString());
+            var displayAttribute = memberInfo[0].GetCustomAttributes(typeof(System.ComponentModel.DataAnnotations.DisplayAttribute), false).FirstOrDefault() as System.ComponentModel.DataAnnotations.DisplayAttribute;
+            return displayAttribute?.Order ?? 0;
+        }
+
+        /// <summary>
+        /// 取得列舉的 DisplayAttribute.ShortName
+        /// </summary>
+        /// <param name="enumValue"></param>
+        /// <returns></returns>
+        public static string? GetShortName(this Enum enumValue)
+        {
+            var enumType = enumValue.GetType();
+            var memberInfo = enumType.GetMember(enumValue.ToString());
+            var displayAttribute = memberInfo[0].GetCustomAttributes(typeof(System.ComponentModel.DataAnnotations.DisplayAttribute), false).FirstOrDefault() as System.ComponentModel.DataAnnotations.DisplayAttribute;
+            return displayAttribute?.ShortName;
+        }
+
+        /// <summary>
+        /// 取得列舉的 DisplayAttribute.Prompt
+        /// </summary>
+        /// <param name="enumValue"></param>
+        /// <returns></returns>
+        public static string? GetPrompt(this Enum enumValue)
+        {
+            var enumType = enumValue.GetType();
+            var memberInfo = enumType.GetMember(enumValue.ToString());
+            var displayAttribute = memberInfo[0].GetCustomAttributes(typeof(System.ComponentModel.DataAnnotations.DisplayAttribute), false).FirstOrDefault() as System.ComponentModel.DataAnnotations.DisplayAttribute;
+            return displayAttribute?.Prompt;
+        }
+
+        /// <summary>
+        /// 取得列舉的 DisplayAttribute.AutoGenerateField
+        /// </summary>
+        /// <param name="enumValue"></param>
+        /// <returns></returns>
+        public static bool GetAutoGenerateField(this Enum enumValue)
+        {
+            var enumType = enumValue.GetType();
+            var memberInfo = enumType.GetMember(enumValue.ToString());
+            var displayAttribute = memberInfo[0].GetCustomAttributes(typeof(System.ComponentModel.DataAnnotations.DisplayAttribute), false).FirstOrDefault() as System.ComponentModel.DataAnnotations.DisplayAttribute;
+            return displayAttribute?.AutoGenerateField ?? false;
+        }
+
+        /// <summary>
+        /// 取得列舉的 DisplayAttribute.AutoGenerateFilter
+        /// </summary>
+        /// <param name="enumValue"></param>
+        /// <returns></returns>
+        public static bool GetAutoGenerateFilter(this Enum enumValue)
+        {
+            var enumType = enumValue.GetType();
+            var memberInfo = enumType.GetMember(enumValue.ToString());
+            var displayAttribute = memberInfo[0].GetCustomAttributes(typeof(System.ComponentModel.DataAnnotations.DisplayAttribute), false).FirstOrDefault() as System.ComponentModel.DataAnnotations.DisplayAttribute;
+            return displayAttribute?.AutoGenerateFilter ?? false;
+        }
+
+        /// <summary>
+        /// 取得列舉清單
+        /// </summary>
+        /// <param name="enumValue"></param>
+        /// <returns></returns>
+        public static IEnumerable<Enum> GetEnumList(this Enum enumValue)
+        {
+            var enumType = enumValue.GetType();
+            return Enum.GetValues(enumType).Cast<Enum>();
+        }
+
+        /// <summary>
+        /// 取得列舉清單的 DisplayAttribute.Name
+        /// </summary>
+        /// <param name="enumValue"></param>
+        /// <returns></returns>
+        public static IEnumerable<string> GetEnumListDisplayNames(this Enum enumValue)
+        {
+            var enumType = enumValue.GetType();
+            var enumValues = Enum.GetValues(enumType).Cast<Enum>();
+
+            if (enumValues == null)
+            {
+                return new List<string>();
+            }
+
+            // 如果 GetDisplayName() 回傳 null，則 throw exception
+            return enumValues!.Select(e => e.GetDisplayName() ?? throw new Exception("DisplayAttribute.Name is null"));
+        }
+
+        
+    }
+}
---
+++ StartFMS.Enums/Repository/SystemNames.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StartFMS.Enums.Repository
+{
+    public enum SystemNames
+    {
+        [Display(Name = "UserRoles", Description = "角色清單")]
+        UserRoles,
+        [Display(Name = "UserAccounts", Description = "使用者帳號")]
+        UserAccounts,
+        [Display(Name = "SystemCatalogItems", Description = "系統目錄")]
+        SystemCatalogItems
+    }
+}
