a7ce17763df1d0017637f70b1d5a2efa5ef59204
John <information9527@gmail.com>
2025-08-10 15:48:46 +0800

    Refactor TraceIdMiddleware to format TraceId and log request headers

--- StartFMS.Backend.API/TraceIdMiddleware.cs
+++ StartFMS.Backend.API/TraceIdMiddleware.cs
@@ -3,6 +3,7 @@ using System;
 using System.Linq;
 using System.Threading.Tasks;
 using Serilog.Context;
+using Serilog;
 
 public class TraceIdMiddleware
 {
@@ -19,13 +20,17 @@ public class TraceIdMiddleware
         var traceId = context.Request.Headers[TraceKey].FirstOrDefault();
         if (string.IsNullOrEmpty(traceId))
         {
-            traceId = Guid.NewGuid().ToString();
+            traceId = Guid.NewGuid().ToString("N"); // 無 -
             context.Request.Headers[TraceKey] = traceId;
         }
         context.Response.Headers[TraceKey] = traceId;
 
         using (LogContext.PushProperty(TraceKey, traceId))
         {
+            // 將所有 request headers 寫入 log
+            var headers = string.Join("; ", context.Request.Headers.Select(h => $"{h.Key}={h.Value}"));
+            Log.Information("Request Headers: {Headers}", headers);
+
             await _next(context);
         }
     }
